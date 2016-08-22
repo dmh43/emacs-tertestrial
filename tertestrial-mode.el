13a282ee40eaea2886c47385f45eeca2cb8305dd
Dany Haddad <danyhaddad43@gmail.com>
2016-08-22 11:19:38 -0700

    add autotest functionality

--- tertestrial-mode.el
+++ tertestrial-mode.el
@@ -1,6 +1,12 @@
 (require 'emacs-tertestrial)
 (require 'emacs-tertestrial-buttercup)
 
+(defun tertestrial-toggle-autosave-hook ()
+  (interactive)
+  (if (advice-member-p 'tertestrial-last-test 'save-buffer)
+      (advice-remove 'save-buffer 'tertestrial-last-test)
+    (advice-add 'save-buffer :after 'tertestrial-last-test)))
+
 (define-minor-mode tertestrial-mode
   "Start tertestrial for the current project and enable keybindings."
   :keymap (let ((map (make-sparse-keymap)))
@@ -11,6 +17,7 @@
             (define-key map (kbd "C-c C-t d") 'tertestrial-test-dir)
             (define-key map (kbd "C-c C-t C-t") 'tertestrial-last-test)
             (define-key map (kbd "C-c C-t c") 'tertestrial-set-actionset)
+            (define-key map (kbd "C-c C-t a") 'tertestrial-toggle-autosave-hook)
             map))
 
 (provide 'tertestrial-mode)
