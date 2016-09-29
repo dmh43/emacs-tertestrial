899a154fb4b19d51f5bdec61afa24c0b5d406294
Dany Haddad <danyhaddad43@gmail.com>
2016-09-28 17:29:02 -0700

    add make sure to save before testing

--- tertestrial-mode.el
+++ tertestrial-mode.el
@@ -1,11 +1,13 @@
-(require 'emacs-tertestrial)
-(require 'emacs-tertestrial-buttercup)
-
-(defun tertestrial-toggle-autosave-hook ()
+(defun tertestrial-toggle-autotest-hook ()
   (interactive)
   (if (advice-member-p 'tertestrial-last-test 'save-buffer)
       (advice-remove 'save-buffer 'tertestrial-last-test)
     (advice-add 'save-buffer :after 'tertestrial-last-test)))
+
+(advice-add 'tertestrial-write-command :before
+            (lambda ()
+              (when (not (advice-member-p 'tertestrial-last-test 'save-buffer))
+                  'save-buffer)))
 
 (define-minor-mode tertestrial-mode
   "Start tertestrial for the current project and enable keybindings."
@@ -17,7 +19,5 @@
             (define-key map (kbd "C-c C-t d") 'tertestrial-test-dir)
             (define-key map (kbd "C-c C-t C-t") 'tertestrial-last-test)
             (define-key map (kbd "C-c C-t c") 'tertestrial-set-actionset)
-            (define-key map (kbd "C-c C-t a") 'tertestrial-toggle-autosave-hook)
+            (define-key map (kbd "C-c C-t a") 'tertestrial-toggle-autotest-hook)
             map))
-
-(provide 'tertestrial-mode)
